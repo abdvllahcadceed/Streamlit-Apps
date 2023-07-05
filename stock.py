8fca20982ea94dce1c7ae08e23c5068d1b9aa7ca
Abdullahi Mohamud <59407972+abdvllahcadceed@users.noreply.github.com>
2023-07-05 18:28:53 +0300

    Add files via upload

---
+++ stock.py
@@ -0,0 +1,26 @@
+# Importing & Loading Packages and Libraries
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.markdown('''
+# **Stock Price App**
+
+
+Application built in `Python` + `Streamlit` + `GitHub` by [Abdullahi M. Cadceed](https://twitter.com/@abdullahcadceed)
+
+---
+''')
+
+# define the ticker symbol
+tickerSymbol = 'GOOGL'
+
+# get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+# get the historical prices for this ticker
+tickerDf = tickerData.history(period='id', start='2010-1-1', end='2022-12-31')
+
+# Open high low close volume dividends stock splits
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
