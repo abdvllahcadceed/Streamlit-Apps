d36442c4024352242719e3a5bfb905c4e157dccf
Abdullahi Mohamud <59407972+abdvllahcadceed@users.noreply.github.com>
2023-07-12 12:24:09 +0300

    Update dna.py

--- dna.py
+++ dna.py
@@ -22,7 +22,7 @@ This App counts the nucleotide composition of your DNA Sequence
 st.header('Enter Your DNA sequence')
 
 sequence_input = ""
-sequence = st.text_area("Sequence Input", sequence_input, height=250)
+sequence = st.text_area("Input Sequence & Press Ctrl+Enter to Apply", sequence_input, height=250)
 sequence = sequence.splitlines()
 sequence = ''.join(sequence)
 
