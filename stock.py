fad6974f3a17695864e3c624760af15638501f1b
Abdullahi Mohamud <59407972+abdvllahcadceed@users.noreply.github.com>
2023-07-12 10:46:40 +0300

    Update stock.py

--- stock.py
+++ stock.py
@@ -8,8 +8,6 @@ st.markdown('''
 Application built in `Python` + `Streamlit` + `GitHub` by [Abdullahi M. Cadceed](https://twitter.com/@abdullahcadceed)
 
 
-
- 
 Shown are the stock **closing price** and ***volume*** of Google!
 
 ---
