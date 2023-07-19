f8f754de89f7efc9c977275ecaabc60980237c41
Abdullahi Mohamud <59407972+abdvllahcadceed@users.noreply.github.com>
2023-07-19 17:05:48 +0300

    Update basketball.py

--- basketball.py
+++ basketball.py
@@ -72,8 +72,8 @@ if st.button('Click Here to See the Heatmap Visualization'):
 			df = df.drop(column, axis=1)
 	
     corr = df.corr()
-    mask = np.zeros_like(corr)
-    mask[np.triu_indices_from(mask)] = True
+	mask = np.zeros_like(corr)
+	mask[np.triu_indices_from(mask)] = True
     with sns.axes_style("white"):
         fig, ax = plt.subplots(figsize=(7, 5))
         ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
