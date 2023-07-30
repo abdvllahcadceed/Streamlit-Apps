e7cb370d4961d5605e76ad67f22fe3b5803224b2
Abdullahi Mohamud <59407972+abdvllahcadceed@users.noreply.github.com>
2023-07-30 16:08:18 +0300

    Create translator.py

---
+++ translator.py
@@ -0,0 +1,14 @@
+from google_trans_new import google_translator
+import streamlit as st
+
+translator = google_translator()
+
+st.title("Simple Somali to English Web App Translator")
+
+st.markdow("""
+Application built in `Python` + `Streamlit` + `GitHub` by [Abdullahi M. Cadceed](https://twitter.com/@abdullahcadceed)
+""")
+
+text = st.text_input("Type Your Text Here & Press ENTER!")
+translate = translator.translate(text,lang_tgt='so')
+st.write(translate)
