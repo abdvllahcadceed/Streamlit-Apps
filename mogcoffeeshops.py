5fb6fd0a875082a256a383e4dd26db6166ec24ae
Abdullahi Mohamud <59407972+abdvllahcadceed@users.noreply.github.com>
2023-08-06 11:40:51 +0300

    Update mogcoffeeshops.py

--- mogcoffeeshops.py
+++ mogcoffeeshops.py
@@ -17,7 +17,7 @@ coffeeShops = pd.DataFrame({
     'Latitude': [2.033, 2.037, 2.030, 2.036, 2.032],
     'Longitude': [45.313, 45.302, 45.303, 45.305, 45.315],
     'Cuisine': ['Shaah', 'Cunno', 'Cabitaan', 'Daango', 'Qaaci'],
-    'Rating': [4.5, 4.7, 4.4, 4.3, 4.8]
+    'Rating': [4.50, 4.70, 4.40, 4.30, 4.80]
 })
 
 # Function to create an interactive map
